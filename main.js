e3df020850aa6facd4e7e1037ec5f8a3fcc24855
maria <saprina.mariya@mail.ru>
2019-09-27 20:54:48 +0300

    footer

--- main.js
+++ main.js
@@ -7,3 +7,21 @@ document.querySelector('.burger_menu').addEventListener('click', () => {
 document.querySelector('#mobile-menu-close-btn').addEventListener('click', () => {
     mobileMenu.style.display = 'none';
 });
+
+
+var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
